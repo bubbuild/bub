0e0541c11ca2a915a33195b12dcaa553d9248144
Frost Ming <me@frostming.com>
2026-04-13 16:56:20 +0800

    feat: dynamic version from VCS and release notes auto-generation
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/bub/__init__.py
+++ src/bub/__init__.py
@@ -1,8 +1,19 @@
 """Bub framework package."""
 
+from importlib import import_module
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as metadata_version
+
 from bub.framework import BubFramework
 from bub.hookspecs import hookimpl
 from bub.tools import tool
 
 __all__ = ["BubFramework", "hookimpl", "tool"]
-__version__ = "0.3.5"
+
+try:
+    __version__ = import_module("bub._version").version
+except ModuleNotFoundError:
+    try:
+        __version__ = metadata_version("bub")
+    except PackageNotFoundError:
+        __version__ = "0.0.0"
